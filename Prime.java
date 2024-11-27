0463662835149f51dfdfc2ca610a6cf90b0e745c
manaswini-2403 <sangepumanu24@gmail.com>
2024-11-27 14:44:13 +0530

    five commit

--- Prime.java
+++ Prime.java
@@ -11,7 +11,7 @@ public class Prime {
       }
 
       if (isPrime) {
-          System.out.println(num + " is a prime number...");
+          System.out.println(num + " is a prime numberrr...");
       } else {
           System.out.println(num + " is not a primeee numberr.");
         }
